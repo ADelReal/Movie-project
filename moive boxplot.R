ac608414b9f3a31c6be1f021f2c3d6e9e64a1f8b
Andrew Del Real <drewrob461@gmail.com>
2021-04-08 21:51:05 -0700

    Add files via upload

---
+++ moive boxplot.R
@@ -0,0 +1,16 @@
+
+#import "Movies-Ratings.csv"
+movies <- read.csv(file.choose())
+
+# checks the data in the CSV file
+head(movies)
+head(movies)
+tail(movies)
+str(movies)
+summary(movies)
+
+# renames the columns
+colnames(movies) <- c("Film", "Genre", "CriticRating", "AudienceRating", "BudgetMillions", "Year")
+
+box <- ggplot(data = movies, aes(x = Genre, y = AudienceRating, color = Genre))
+box + geom_jitter() + geom_boxplot(size = 1.2, alpha = 0.5)
