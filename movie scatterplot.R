cc5cd6239b2208dde3e4c90e701c1af6224caa18
Andrew Del Real <drewrob461@gmail.com>
2021-04-08 21:19:51 -0700

    Update movie scatterplot.R

--- movie scatterplot.R
+++ movie scatterplot.R
@@ -1,6 +1,19 @@
 #import "Movies-Ratings.csv"
 movies <- read.csv(file.choose())
 
+# checks the data in the CSV file
+head(movies)
+head(movies)
+tail(movies)
+str(movies)
+summary(movies)
+
+# renames the columns
+colnames(movies) <- c("Film", "Genre", "CriticRating", "AudienceRating", "BudgetMillions", "Year")
+
+istall.package('ggplot2')
+
+#activates ggplot2
 library(ggplot2)
 
 ggplot(data = movies, aes(x = CriticRating, y= AudienceRating, 
